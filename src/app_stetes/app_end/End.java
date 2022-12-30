defc9aa1c0a0ed0630cd9ca3996ee80a656f4920
bbybaobao <cozykamiii@gmail.com>
2022-12-30 04:59:38 +0200

    Add values

--- src/app_stetes/app_end/End.java
+++ src/app_stetes/app_end/End.java
@@ -4,5 +4,7 @@ public class End {
     public static void main(String[] args) {
         System.out.println("Converter for base converting");
         System.out.println("Version 1.0");
+        double miles = 15;
+        double kms = 26;
     }
 }
