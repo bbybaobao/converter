8318d0907fdb2a68399678535a7b5102da1927c7
bbybaobao <cozykamiii@gmail.com>
2022-12-30 04:58:39 +0200

    Add otput End

--- src/app_stetes/app_end/End.java
+++ src/app_stetes/app_end/End.java
@@ -1,4 +1,8 @@
 package app_stetes.app_end;
 
 public class End {
+    public static void main(String[] args) {
+        System.out.println("Converter for base converting");
+        System.out.println("Version 1.0");
+    }
 }
